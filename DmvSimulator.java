0ea652cd9957101e226f9beb82e037355456c940
Henokk-14 <>
2021-03-02 12:03:04 -0500

    Dmv ticketing system

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -1,4 +1,18 @@
+import java.lang.Math;
 public class DmvSimulator {
-    public static void main(String[] args) {
-    
+  public static void main(String[] args) {
+    System.out.println("Welcom to the DMV :)");
+    int ticketNum = (int) (Math.random()*100);
+    System.out.println("Take your number please wait until it is called your number is "+ ticketNum);
+    int wrapNum = ticketNum + 1;
+    while (ticketNum != wrapNum){
+      System.out.println("now calling "+ wrapNum);
+      wrapNum++;
+      if (wrapNum == 100){
+        wrapNum = 1;
+      }
     }
+    System.out.println("now calling "+ ticketNum);
+    System.out.println("Sorry you don't have the required documents");
+  }
+}
