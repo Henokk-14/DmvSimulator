43359dd4cf8b78797385660dac72c2e128d49092
Henokk-14 <>
2021-03-02 12:11:10 -0500

    increased num of tickets

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -2,13 +2,13 @@ import java.lang.Math;
 public class DmvSimulator {
   public static void main(String[] args) {
     System.out.println("Welcom to the DMV :)");
-    int ticketNum = (int) (Math.random()*100);
+    int ticketNum = (int) (Math.random()*200);
     System.out.println("Take your number please wait until it is called your number is "+ ticketNum);
     int wrapNum = ticketNum + 1;
     while (ticketNum != wrapNum){
       System.out.println("now calling "+ wrapNum);
       wrapNum++;
-      if (wrapNum == 100){
+      if (wrapNum == 201){
         wrapNum = 1;
       }
     }
